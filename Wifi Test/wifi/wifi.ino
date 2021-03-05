8972791a0e219dde9a73072392d6144b2d7c98fd
Mihai <mihai.anghelin@gmail.com>
2021-03-05 17:13:33 +0200

    Wifi Module Test

---
+++ Wifi Test/wifi/wifi.ino
@@ -0,0 +1,27 @@
+#include <SoftwareSerial.h>
+#define port 9600
+
+SoftwareSerial ESPserial(4, 5); // RX | TX
+
+void setup()
+{
+  Serial.begin(port);
+  ESPserial.begin(port);
+
+  Serial.println("");
+  Serial.println("Remember to to set Both NL & CR in the serial monitor.");
+  Serial.println("Ready");
+  Serial.println("");
+}
+
+void loop()
+{
+  if ( ESPserial.available() )
+  {
+    Serial.write( ESPserial.read() );
+  }
+
+  if ( Serial.available() ) {
+    ESPserial.write( Serial.read() );
+  }
+}
